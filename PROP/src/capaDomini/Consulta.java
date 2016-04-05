d710677df8b4c4c2a20c5e42f99e1275672861ef
rogiia <rogeroriwd@gmail.com>
2016-04-06 01:14:11 +0200

    Declarat funcions

---
+++ PROP/src/capaDomini/Consulta.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package capaDomini;
+
+/**
+ *
+ * @author rogia
+ */
+public class Consulta implements Cloneable {
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+}
