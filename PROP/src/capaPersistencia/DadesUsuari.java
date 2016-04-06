ad29b3952eaef15d6378374611f70c1edfa340e5
aleixap88 <aleixabrieprat@macbookprodealeix.local>
2016-04-06 19:37:10 +0200

    ExisteixUsuari_contrasenya(object), ExisteixUsusari

--- PROP/src/capaPersistencia/DadesUsuari.java
+++ PROP/src/capaPersistencia/DadesUsuari.java
@@ -6,10 +6,12 @@
 package capaPersistencia;
 
 import capaDomini.Usuaris.UsuariNormal;
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
@@ -21,7 +23,7 @@ import java.io.PrintWriter;
  */
 public class DadesUsuari {
     
-    private String ruta_BD;
+    private final static String nom_BD = "BD_users.txt";
     
     
     public DadesUsuari() {
@@ -31,27 +33,31 @@ public class DadesUsuari {
     public static void main(String[] args) throws IOException {
         DadesUsuari dades = new DadesUsuari();
         dades.crea_arxiu();
-        String user_name = "aaaaa";
-        String password = "bbbbbbbbb";
+        String user_name = "Anselmo";
+        String password = "bbb";
         UsuariNormal usn;
         usn = new UsuariNormal(user_name,password);
         GuardarUsuari(usn);
+        if (ExisteixUsuari_contrasenya(usn)) System.out.println("EXISTEIX");
+        else System.out.println("NO EXISTEIX");
     }
     
-    public static Boolean existeix_arxiu() {
-        File archivo = new File("BD_users.txt");
+    /*
+    public Boolean existeix_arxiu() {
+        File archivo = new File(nom_BD);
         if (archivo.exists()) return true;
         else return false;
     }
+    */
     
     public static void GuardarUsuari(UsuariNormal us){
         FileWriter fichero = null;
         PrintWriter pw = null;
         try
         {
-            fichero = new FileWriter("BD_users.txt",true);
+            fichero = new FileWriter(nom_BD,true);
             pw = new PrintWriter(fichero);
-            pw.print(us.consultar_username()+"  "+us.consultar_password());
+            pw.println(us.consultar_username()+"  "+us.consultar_password());
         } catch (Exception e) {
             
         } finally {
@@ -64,6 +70,89 @@ public class DadesUsuari {
               
            }
         }
+    }
+    
+    public static Boolean ExisteixUsuari_contrasenya(UsuariNormal usn) {
+        File archivo = null;
+      FileReader fr = null;
+      BufferedReader br = null;
+      try {
+         // Apertura del fichero y creacion de BufferedReader para poder
+         // hacer una lectura comoda (disponer del metodo readLine()).
+         archivo = new File (nom_BD);
+         fr = new FileReader (archivo);
+         br = new BufferedReader(fr);
+
+         // Lectura del fichero
+         String linea;
+         String aux = "";
+         while((linea=br.readLine())!=null) {
+            int i = 0;
+            while (linea.charAt(i) != ' ') ++i;
+             aux = linea.substring(0, i);
+             System.out.println(aux);
+            if (usn.consultar_username().equals(aux)) {
+                i = i+2;
+                aux = linea.substring(i,linea.length());
+                System.out.println(aux);
+                return true;
+            }
+         }
+      }
+      catch(Exception e){
+          
+      }
+      finally{
+         // En el finally cerramos el fichero, para asegurarnos
+         // que se cierra tanto si todo va bien como si salta 
+         // una excepcion.
+         try{                    
+            if( null != fr ){   
+               fr.close();     
+            }                  
+         }catch (Exception e2){ 
+         }
+      }
+      return false;
+    }
+    
+    public static Boolean ExisteixUsuari(String username) {
+      File archivo = null;
+      FileReader fr = null;
+      BufferedReader br = null;
+      try {
+         // Apertura del fichero y creacion de BufferedReader para poder
+         // hacer una lectura comoda (disponer del metodo readLine()).
+         archivo = new File (nom_BD);
+         fr = new FileReader (archivo);
+         br = new BufferedReader(fr);
+
+         // Lectura del fichero
+         String linea;
+         String aux = "";
+         while((linea=br.readLine())!=null) {
+            int i = 0;
+            while (linea.charAt(i) != ' ') ++i;
+             aux = linea.substring(0, i);
+             System.out.println(aux);
+            if (username.equals(aux)) return true;
+         }
+      }
+      catch(Exception e){
+          
+      }
+      finally{
+         // En el finally cerramos el fichero, para asegurarnos
+         // que se cierra tanto si todo va bien como si salta 
+         // una excepcion.
+         try{                    
+            if( null != fr ){   
+               fr.close();     
+            }                  
+         }catch (Exception e2){ 
+         }
+      }
+      return false;
     }
     
     public void crea_arxiu() throws IOException {
