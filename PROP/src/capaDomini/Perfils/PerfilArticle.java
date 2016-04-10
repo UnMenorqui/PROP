df165234e1720162c477dae63b200742fc83c076
marcanglada1@gmail.com <anglada468750>
2016-04-10 23:31:03 +0200

    Article - Autor

--- PROP/src/capaDomini/Perfils/PerfilArticle.java
+++ PROP/src/capaDomini/Perfils/PerfilArticle.java
@@ -18,18 +18,64 @@ import java.util.Vector;
  */
 public class PerfilArticle extends Perfil {
     
-    List<String> aux = new Vector<String>();
+    List<String> termes = new Vector<String>();
+    List<String> conferencies = new Vector<String>();
+    List<String> autors = new Vector<String>();
+    int quantitat_termes;
+    int quantitat_conf;
+    int quantitat_autors;
     
-    public List<String> autors_importants(int quantitat) {
-        return aux;
+    
+    public int get_quantitat_termes() {
+        return quantitat_termes;
     }
     
-    public List<String> conferencies_importants(int quantitat) {
-        return aux;
+    public int get_quantitat_conf() {
+        return quantitat_conf;
     }
     
-    public List<String> termes_importants(int quantitat) {
-        return aux;
+    public int get_quantitat_autors() {
+        return quantitat_autors;
+    }
+    
+    public List<String> autors_importants() {
+        // obtenir_autors(autors,nom,quantitat_autors);
+        return autors;
+    }
+    
+    public List<String> conferencies_importants() {
+        // obtenir_conferencies(conferencies,nom,quantitat_conf);
+        return conferencies;
+    }
+    
+    public List<String> termes_importants() {
+        // obtenir_termes(termes,nom,quantitat_termes);
+        return termes;
+    }
+    
+    public void crear_perfil() {
+        autors_importants();
+        conferencies_importants();
+        termes_importants();
+        System.out.println(nom);
+        System.out.println("");
+        System.out.println("Conferencies més rellevants d'aquest article:");
+        for (int i = 0; i < conferencies.size(); ++i) {
+            String aux = conferencies.get(i);
+            System.out.println(aux);
+        }
+        System.out.println("");
+        System.out.println("Autors més rellevants d'aquest article:");
+        for (int i = 0; i < autors.size(); ++i) {
+            String aux = autors.get(i);
+            System.out.println(aux);
+        }
+        System.out.println("");
+        System.out.println("Termes més rellevants d'aquest article:");
+        for (int i = 0; i < termes.size(); ++i) {
+            String aux = termes.get(i);
+            System.out.println(aux);
+        }
     }
     
 }
--- PROP/src/capaDomini/Perfils/PerfilAutor.java
+++ PROP/src/capaDomini/Perfils/PerfilAutor.java
@@ -16,20 +16,66 @@ import java.util.Vector;
  *
  * @author marc_
  */
-public class PerfilAutor extends Perfil { 
+public class PerfilAutor extends Perfil {
     
-    List<String> aux = new Vector<String>(); 
+    List<String> termes = new Vector<String>();
+    List<String> conferencies = new Vector<String>();
+    List<String> articles = new Vector<String>();
+    int quantitat_termes;
+    int quantitat_conf;
+    int quantitat_articles;
     
-    public List<String> articles_importants(int quantitat) {
-        return aux;
+    
+    public int get_quantitat_termes() {
+        return quantitat_termes;
     }
     
-    public List<String> conferencies_importants(int quantitat) {
-         return aux;
+    public int get_quantitat_conf() {
+        return quantitat_conf;
     }
     
-    public List<String> termes_importants(int quantitat) {
-         return aux;
+    public int get_quantitat_articles() {
+        return quantitat_articles;
+    }
+    
+    public List<String> articles_importants() {
+        // obtenir_autors(autors,nom,quantitat_autors);
+        return articles;
+    }
+    
+    public List<String> conferencies_importants() {
+        // obtenir_conferencies(conferencies,nom,quantitat_conf);
+        return conferencies;
+    }
+    
+    public List<String> termes_importants() {
+        // obtenir_termes(termes,nom,quantitat_termes);
+        return termes;
+    }
+    
+    public void crear_perfil() {
+        articles_importants();
+        conferencies_importants();
+        termes_importants();
+        System.out.println(nom);
+        System.out.println("");
+        System.out.println("Conferencies més rellevants d'aquest autor:");
+        for (int i = 0; i < conferencies.size(); ++i) {
+            String aux = conferencies.get(i);
+            System.out.println(aux);
+        }
+        System.out.println("");
+        System.out.println("Articles més rellevants d'aquest autor:");
+        for (int i = 0; i < articles.size(); ++i) {
+            String aux = articles.get(i);
+            System.out.println(aux);
+        }
+        System.out.println("");
+        System.out.println("Termes més rellevants d'aquest autor:");
+        for (int i = 0; i < termes.size(); ++i) {
+            String aux = termes.get(i);
+            System.out.println(aux);
+        }
     }
     
 }
