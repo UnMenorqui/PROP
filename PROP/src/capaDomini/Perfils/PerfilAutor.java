dcf165eba8d10485887beb94877915c794175679
Aleixap88 <aleixap88@gmail.com>
2016-04-11 16:26:16 +0200

    pijades varies

--- PROP/src/capaDomini/Perfils/PerfilAutor.java
+++ PROP/src/capaDomini/Perfils/PerfilAutor.java
@@ -7,7 +7,7 @@ package capaDomini.Perfils;
 
 
 import java.util.List;
-import java.util.Vector;
+import java.util.ArrayList;
 
 /**
  *
@@ -15,9 +15,9 @@ import java.util.Vector;
  */
 public class PerfilAutor extends Perfil {
     
-    static List<String> termes = new Vector<String>();
-    static List<String> conferencies = new Vector<String>();
-    static List<String> articles = new Vector<String>();
+    static ArrayList<String> termes = new ArrayList<>();
+    static ArrayList<String> conferencies = new ArrayList<>();
+    static ArrayList<String> articles = new ArrayList<>();
     static int quantitat_termes;
     static int quantitat_conf;
     static int quantitat_articles;
@@ -35,17 +35,17 @@ public class PerfilAutor extends Perfil {
         return quantitat_articles;
     }
     
-    public static List<String> articles_importants() {
+    public static ArrayList<String> articles_importants() {
         // obtenir_articles(articles,nom,quantitat_articles);
         return articles;
     }
     
-    public static List<String> conferencies_importants() {
+    public static ArrayList<String> conferencies_importants() {
         // obtenir_conferencies(conferencies,nom,quantitat_conf);
         return conferencies;
     }
     
-    public static List<String> termes_importants() {
+    public static ArrayList<String> termes_importants() {
         // obtenir_termes(termes,nom,quantitat_termes);
         return termes;
     }
