e9226f49658b92a41ba31c1ad43688c2fdcaea88
UnMenorqui <toniriera95@gmail.com>
2016-04-12 23:23:49 +0200

    Creacio de maps

--- PROP/src/capaDomini/consulta/Consulta.java
+++ PROP/src/capaDomini/consulta/Consulta.java
@@ -1,12 +1,56 @@
 package capaDomini.consulta;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  *
  * @author aleixabrieprat
  */
 public class Consulta {
+    
+    
+    /*
+        El primer Integer de cada map correspon a l'identificador de
+        cada autor,conferència,terme o article.
+    
+        El segon Integer de cada map correspon a la prioritat de cada
+        un dels autors,conferències, termes o articles.
+    
+        La prioritat ens serivirà per a buscar la informació més important.
+        Aquesta, és la quantitat d'adjacències del node + cops consultat.
+    
+    */
+    
+    
+    Map<Integer, Integer> Map_autors = new HashMap<>();
+    
+    Map<Integer, Integer> Map_conf = new HashMap<>();
+    
+    Map<Integer, Integer> Map_terme = new HashMap<>();
+    
+    Map<Integer, Integer> Map_article = new HashMap<>();
+    
+    
+    private void omplir_map_autors() {
+        
+    }
+    
+    private void omplir_map_conferencies() {
+        
+    }
+    
+    private void omplir_map_terme() {
+        
+    }
+    
+    private void omplir_map_article() {
+        
+    }
+    
+    
+    
     
     public void obtenir_autors(ArrayList<String> autors, String nom, int quantitat) {
     }
@@ -16,5 +60,4 @@ public class Consulta {
     }
     public void obtenir_articles(ArrayList<String> autors, String nom, int quantitat) {
     }
-    
 }
