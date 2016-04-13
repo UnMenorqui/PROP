63f31e70f67e13a7a74e88143d5aa769850c0471
UnMenorqui <toniriera95@gmail.com>
2016-04-13 20:06:04 +0200

    Omplir GRaf CABRONS FEIS FEINA

--- PROP/src/capaDomini/Graf/DriverGraf.java
+++ PROP/src/capaDomini/Graf/DriverGraf.java
@@ -5,10 +5,63 @@
  */
 package capaDomini.Graf;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  *
  * @author Toni
  */
 public class DriverGraf {
+    
+    private Map<Integer,Double> node = new HashMap<Integer,Double>();
+    
+    private Map<Integer, Map<Integer,Double> > Matriu = new HashMap<Integer, Map<Integer,Double>>();
+    
+    public void main (String args[]) {
+        node.put(100, 1.0);
+        node.put(101,1.0);
+        node.put(70, 1.0);
+        node.put(51,1.0);
+        node.put(102,0.0);
+        node.put(103,0.0);
+        node.put(52,0.0);
+        node.put(104,0.0);
+        node.put(72,0.0);
+        node.put(71,0.0);
+        Matriu.put(1, node);
+        
+        
+        node.clear();
+        node.put(100, 0.0);
+        node.put(101,0.0);
+        node.put(70, 0.0);
+        node.put(51,1.0);
+        node.put(102,1.0);
+        node.put(103,0.0);
+        node.put(52,0.0);
+        node.put(104,0.0);
+        node.put(72,0.0);
+        node.put(71,1.0);
+        Matriu.put(2, node);
+        
+        
+        node.clear();
+        node.put(100, 1.0);
+        node.put(101,0.0);
+        node.put(70, 0.0);
+        node.put(51,0.0);
+        node.put(102,0.0);
+        node.put(103,1.0);
+        node.put(52,1.0);
+        node.put(104,1.0);
+        node.put(72,1.0);
+        node.put(71,0.0);
+        Matriu.put(3, node);
+        
+        
+        
+        
+    }
     
 }
