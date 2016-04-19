a2600a2bd76f692212ed2b85522f1a519a806c50
UnMenorqui <toniriera95@gmail.com>
2016-04-19 20:42:19 +0200

    fora maps de consulta

--- PROP/src/capaDomini/consulta/Consulta.java
+++ PROP/src/capaDomini/consulta/Consulta.java
@@ -11,48 +11,8 @@ import java.util.Map;
 public class Consulta {
     
     
-    /*
-        El primer Integer de cada map correspon a l'identificador de
-        cada autor,conferència,terme o article.
-    
-        El segon Integer de cada map correspon a la prioritat de cada
-        un dels autors,conferències, termes o articles.
-    
-        La prioritat ens serivirà per a buscar la informació més important.
-        Aquesta, és la quantitat d'adjacències del node + cops consultat.
-    
-    */
-    
-    
-    Map<Integer, Integer> Map_autors = new HashMap<>();
-    
-    Map<Integer, Integer> Map_conf = new HashMap<>();
-    
-    Map<Integer, Integer> Map_terme = new HashMap<>();
-    
-    Map<Integer, Integer> Map_article = new HashMap<>();
-    
-    
-    private void omplir_map_autors() {
-        
-    }
-    
-    private void omplir_map_conferencies() {
-        
-    }
-    
-    private void omplir_map_terme() {
-        
-    }
-    
-    private void omplir_map_article() {
-        
-    }
-    
-    
-    
-    
     public void obtenir_autors(ArrayList<String> autors, String nom, int quantitat) {
+        
     }
     public void obtenir_termes(ArrayList<String> autors, String nom, int quantitat) {
     }
