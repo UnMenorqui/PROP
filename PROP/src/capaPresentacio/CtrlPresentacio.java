3f8aa56bbd6ae14d961371b47efb8633fd9f3c4a
UnMenorqui <toniriera95@gmail.com>
2016-05-17 15:26:40 +0200

    laast

--- PROP/src/capaPresentacio/CtrlPresentacio.java
+++ PROP/src/capaPresentacio/CtrlPresentacio.java
@@ -6,6 +6,12 @@
 package capaPresentacio;
 
 
+import capaDomini.Graf.CtrlGraf;
+import capaDomini.Graf.Node;
+import capaDomini.Perfils.PerfilArticle;
+import capaDomini.Perfils.PerfilAutor;
+import capaDomini.Perfils.PerfilConf;
+import capaDomini.Perfils.PerfilTerme;
 import capaDomini.consulta.Apunts;
 import capaPersistencia.CtrlDadesHistorial;
 import capaDomini.Usuaris.CtrlUsuaris;
@@ -84,16 +90,97 @@ public class CtrlPresentacio {
     
     
     // FUNCIONS CTRL GRAF
+    CtrlGraf cg = new CtrlGraf();
+    public int getidArrayString(String nom,String tipus) {
+        return cg.getidArrayString(nom, tipus);
+    }
+    
+    public Node getNodeIessim(int id, String tipus) {
+        return cg.getNodeIessim(id, tipus);
+    }
+    
+    public int getidArrayint(int id, String tipus) {
+        return cg.getidArrayint(id, tipus);
+    }
+    
+    public String getNomNode(int id, String tipus) {
+        return cg.getNomNode(id, tipus);
+    }
+    
+    public double getValorNode(int id, String tipus) {
+        return cg.getValorNode(id, tipus);
+    }
+    
+    public int GetIDnode(int id, String tipus) {
+        return cg.GetIDnode(id, tipus);
+    }
+    
+    public boolean existeixnode(String nom, String tipus) {
+        return cg.existeixnode(nom, tipus);
+    }
+    
+    public void afegirNode(String tipus,String nom) {
+        cg.afegirNode(tipus, nom);
+    }
+    
+    public void eliminarNode(String nomNode, String tipus) {
+        cg.eliminarNode(nomNode, tipus);
+    }
+    
+    public void eliminarAresta(String nom1, String nom2, String tipus) {
+        cg.eliminarAresta(nom1, nom2, tipus);
+    }
+    
+    public void afegirAresta(String nom1, String nom2, String tipus) {
+        cg.afegirAresta(nom1, nom2, tipus);
+    }
     
     
     
     // FUNCIONS CTRL PERFILS
+    PerfilAutor p_autor = new PerfilAutor();
+    public void crear_perfil_autor(String entitat, int articles, int co_autors, int termes, int conferencies, CtrlGraf G,boolean usuaris)
+    {
+        p_autor.set_name(entitat);
+        p_autor.set_quantitat_articles(articles);
+        p_autor.set_quantitat_autors(co_autors);
+        p_autor.set_quantitat_termes(termes);
+        p_autor.set_quantitat_conf(conferencies);
+        p_autor.crear_perfil_autor(G,usuaris);
+    }
+    
+    PerfilTerme p_terme = new PerfilTerme();
+    public void crear_perfil_terme(String entitat, int articles, int autors, int conferencies, CtrlGraf G, boolean usuaris) 
+    {
+        p_terme.set_name(entitat);
+        p_terme.set_quantitat_articles(articles);
+        p_terme.set_quantitat_autors(autors);
+        p_terme.set_quantitat_conf(conferencies);
+        p_terme.crear_perfil_terme(G,usuaris);
+    }
+    
+    PerfilConf p_conf = new PerfilConf();
+    public void crear_perfil_conferencia(String entitat, int articles, int autors, int termes, CtrlGraf G, boolean usuaris) 
+    {
+        p_conf.set_name(entitat);
+        p_conf.set_quantitat_articles(articles);
+        p_conf.set_quantitat_autors(autors);
+        p_conf.set_quantitat_termes(termes);
+        p_conf.crear_perfil_conf(G,usuaris);
+        
+    }
+    
+    PerfilArticle p_art = new PerfilArticle();
+    public void crear_perfil_article(String entitat, int autors, int termes, int conferencies, CtrlGraf G, boolean usuaris) 
+    {
+        p_art.set_name(entitat);
+        p_art.set_quantitat_autors(autors);
+        p_art.set_quantitat_conf(conferencies);
+        p_art.set_quantitat_termes(termes);
+        p_art.crear_perfil_article(G,usuaris);
+    }  
 
     
     // FUNCIONS CTRL CONSULTA
-    
-    
-    
-    
     
 }
--- PROP/src/capaPresentacio/DataSummaryApplet.java
+++ PROP/src/capaPresentacio/DataSummaryApplet.java
@@ -5,13 +5,22 @@
  */
 package capaPresentacio;
 
+import static com.sun.org.glassfish.external.amx.AMXUtil.prop;
 import netscape.javascript.JSObject;
 import java.applet.Applet;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import netscape.javascript.*; // add plugin.jar to classpath during compilation
 
 public class DataSummaryApplet extends Applet {
     public void start() {
+        prop p = new prop();
+        String[] args = null;
         try {
+            prop.main(args);
+            
+            /*try {
             JSObject window;
             window = JSObject.getWindow(this);
 
@@ -26,20 +35,23 @@ public class DataSummaryApplet extends Applet {
             // get a JavaScript object and retrieve its contents
             JSObject address = (JSObject) window.eval("new address();");
             String addressStr = (String) address.getMember("street") + ", " +
-                    (String) address.getMember("city") + ", " +
-                    (String) address.getMember("state");
-
+            (String) address.getMember("city") + ", " +
+            (String) address.getMember("state");
+            
             // get an array from JavaScript and retrieve its contents
             JSObject phoneNums = (JSObject) window.eval("getPhoneNums()");
             String phoneNumStr = (String) phoneNums.getSlot(0) + ", " +
-                    (String) phoneNums.getSlot(1);
-
+            (String) phoneNums.getSlot(1);
+            
             // dynamically change HTML in page; write data summary
             String summary = userName + " : " + age + " : " +
-                    addressStr + " : " + phoneNumStr;
+            addressStr + " : " + phoneNumStr;
             window.call("writeSummary", new Object[] {summary})   ;
-        } catch (JSException jse) {
+            } catch (JSException jse) {
             jse.printStackTrace();
+            }*/
+        } catch (IOException ex) {
+            Logger.getLogger(DataSummaryApplet.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 }
